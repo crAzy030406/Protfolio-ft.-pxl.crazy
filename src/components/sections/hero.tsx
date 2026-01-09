9c6c350ead1ebf2f3ed99dfc20ae7d09c0953d3a
Crazy <halderardhendu7696@gmail.com>
2026-01-09 20:31:18 +0000

    Increase more by 50% again

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -16,17 +16,17 @@ export default function Hero() {
           <div className="z-10 flex flex-col items-center text-center md:items-start md:text-left">
             
             <div className="relative flex flex-col items-center md:items-start">
-              <span className="md:hidden mb-2 text-xl font-light text-muted-foreground/70">
+              <span className="md:hidden mb-2 text-xl font-light text-muted-foreground/70 opacity-70">
                 I'm
               </span>
               <span
-                className="hidden md:block mb-2 text-xl font-light text-muted-foreground/70 md:font-bold md:text-muted-foreground md:text-2xl md:absolute md:-left-4 md:top-10 md:-rotate-90 md:transform-origin-bottom-left md:mb-0"
+                className="hidden md:block mb-2 text-xl font-light text-muted-foreground/70 md:font-bold md:text-muted-foreground md:text-2xl md:absolute md:-left-8 md:top-10 md:-rotate-90 md:transform-origin-bottom-left md:mb-0"
               >
                 I'm
               </span>
               <div className="flex flex-col md:ml-8 lg:ml-12">
                 <div className="flex flex-col md:flex-row md:items-baseline md:gap-1">
-                  <h1 id="hero-title-1" className="text-5xl sm:text-6xl md:text-8xl lg:text-[10rem] leading-none font-bold font-headline tracking-[0.2em] md:tracking-widest">
+                  <h1 id="hero-title-1" className="text-5xl sm:text-6xl md:text-8xl lg:text-[10rem] leading-none font-bold font-headline tracking-[0.3em] md:tracking-widest">
                     CRAZY
                   </h1>
                   <h1 id="hero-title-2" className="mt-4 md:mt-0 text-lg sm:text-xl md:text-3xl lg:text-4xl font-normal font-headline tracking-[0.2em] md:tracking-tighter text-muted-foreground whitespace-nowrap">
@@ -36,7 +36,7 @@ export default function Hero() {
               </div>
             </div>
 
-            <p className="max-w-lg mt-4 md:mt-6 text-muted-foreground md:text-lg">
+            <p className="max-w-lg mt-4 md:mt-9 text-muted-foreground md:text-lg">
               I'm a passionate designer specializing in creating bold, engaging visuals that tell a story. From branding to digital content, I bring ideas to life with creativity and precision.
             </p>
           </div>
